7e26c1d1b1be0d457d5cf3cb2c8bd08a240273f6
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-11-05 01:03:43 +0400

    Update web_dynamic

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -16,19 +16,19 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route("/0-hbnb/", strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """HBNB is alive!"""
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+    state_count = []
 
     for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+        state_count.append([state, sorted(state.cities, key=lambda k: k.name)])
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
@@ -37,13 +37,15 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     cache_id = uuid.uuid4()
 
-    return render_template('0-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places,
-                           cache_id=cache_id)
+    return render_template(
+        "0-hbnb.html",
+        states=state_count,
+        amenities=amenities,
+        places=places,
+        cache_id=cache_id,
+    )
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    """Main Function"""
+    app.run(host="0.0.0.0", port=5000)
